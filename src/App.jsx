d5427a6be05c42b5247dda8ad18ba3024340aa64
githubscientist <sathish.datascientist@gmail.com>
2026-06-08 20:43:33 +0530

    first class component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
-function App() {
-  return <h1>Hello World!</h1>
+import React from "react";
+
+// inheritance in OOP -> inheriting all the properties
+// or characteristics from one component to another component
+class App extends  React.Component {
+  render() {
+    return <h1>Hello World!</h1>
+  }
 }
 
 export default App;
