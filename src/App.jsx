847ac77851189ce5decb9bd0376a576e448a2f0d
githubscientist <sathish.datascientist@gmail.com>
2026-06-08 20:54:29 +0530

    creating multiple components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+// Header Comonent
+function Header() {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <p>Welcome to React!!</p>
+    </div>
+  )
+}
+
+// Main Component
 function App() {
-  return <h1>Hello World!</h1>
+  return (
+    <>
+      <Header />
+      <Header />
+      <Header />
+      <Header />
+    </>
+  )
 }
 
 export default App;
