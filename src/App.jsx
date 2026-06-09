4088868eb9c7aead53ca67b70e3e4881598dc7d1
githubscientist <sathish.datascientist@gmail.com>
2026-06-09 18:24:21 +0530

    javascript variables in component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import Header from "./components/Header";
 
 // Main Component
 function App() {
+
+  // let's say we have some data
+  let name = 'krish';
+
   return (
     <>
-      <Header />
-      <Header />
-      <Header />
-      <Header />
+      <h1>Hello { name.slice(0, 1).toUpperCase() + name.slice(1, ) }!</h1>
     </>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,6 @@ function Header() {
   return (
     <div>
       <h1>Hello World!</h1>
-      <p>Welcome to React!!</p>
     </div>
   )
 }
