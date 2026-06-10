51dc98065293d6424e9156ef421a53323d54c46f
githubscientist <sathish.datascientist@gmail.com>
2026-06-10 20:28:57 +0530

    complex state management

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,29 @@ const App = () => {
 
   const [likes, setLikes] = useState(0);
   const [dislikes, setDislikes] = useState(0);
+  const [history, setHistory] = useState([]);
+
+  // [L, D, L, D, D, L, L, L]
 
   const handleLike = () => {
     setLikes(likes + 1);
+    // history.push('L');
+    setHistory([
+      ...history,
+      'L'
+    ]);
   }
 
   const handleDislike = () => {
     setDislikes(dislikes + 1);
+    // history.push('D');
+    setHistory([
+      ...history,
+      'D'
+    ]);
   }
+
+  console.log(history);
 
   return (
     <>
