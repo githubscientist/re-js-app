e34a37b1594c5eda5f7b6573fd6d76f6f2b2aff7
githubscientist <sathish.datascientist@gmail.com>
2026-06-10 20:04:26 +0530

    adding dislike to manage the same likes state

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,21 @@ import { useState } from "react";
 
 const App = () => {
 
-  // state management -> introducing state into components
-  // useState hook -> using this hook, we can bring state managment feature
-  // to the functional component
-  // hooks are functions
-  // useState()
-
   const [likes, setLikes] = useState(0);
 
   const handleLike = () => {
-    // update the state likes
-    // always take the current value of likes state
-    // and update it to plus 1 value
     setLikes(likes + 1);
+  }
+
+  const handleDislike = () => {
+    setLikes(likes - 1);
   }
 
   return (
     <>
       <h1>Likes: { likes }</h1>
-      <button onClick={handleLike}>Like</button>
+      <button onClick={handleLike}>Like</button> &nbsp;
+      <button onClick={handleDislike}>Dislike</button>
     </>
   )
 }
