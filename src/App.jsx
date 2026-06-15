717efe6d5b68eaa603292d33d27565e3c6ecea0b
githubscientist <sathish.datascientist@gmail.com>
2026-06-15 19:53:36 +0530

    useEffect with dependencies list

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import { useEffect, useState } from "react";
 const App = () => {
 
   const [likes, setLikes] = useState(0);
+  const [dislikes, setDislikes] = useState(0);
 
-  // useEffect: with dependencies argument but without any dependencies
+  // useEffect: with dependencies argument and with dependencies list
   // runs whenever the component is mounted
-  // lifecycle methods: componentDidMount()
+  // runs whenever the state/props provided as dependencies changes/updates
+  // lifecycle methods: componentDidMount() + componentDidUpdate() --modified
   useEffect(() => {
-    console.log('component is mounted');
-  }, []);
+    console.log('component is mounted and whenever likes state changes');
+  }, [likes]);
 
   const handleLike = () => {
     // this gets executed when the like button is clicked
@@ -21,6 +23,9 @@ const App = () => {
     <div>
       <h1>Likes: { likes }</h1>
       <button onClick={handleLike}>Like</button>
+
+      <h2>Dislikes: {dislikes}</h2>
+      <button onClick={() => setDislikes(dislikes + 1)}>Dislike</button>
     </div>
   )
 }
