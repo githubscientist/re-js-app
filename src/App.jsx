c9680e485d4ea39cf466fe960c08be5973677958
githubscientist <sathish.datascientist@gmail.com>
2026-06-15 19:42:01 +0530

    useEffect without dependencies argument

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,29 @@
-import { Component } from 'react';
-import './App.css';
-import Card from './components/Card';
+import { useEffect, useState } from "react";
 
-export class App extends Component {
-  constructor(props) {
-    super(props);
+const App = () => {
 
-    this.state = {
-      likes: 0,
-      isHidden: false
-    }
+  const [likes, setLikes] = useState(0);
+
+  // useEffect: without dependencies argument
+  // runs whenever the component is mounted
+  // run whenever the component gets updated
+  // lifecycle methods: componentDidMount() + componentDidUpdate()
+  useEffect(() => {
+    console.log('component is mounted/updated');
+  });
+
+  const handleLike = () => {
+    // this gets executed when the like button is clicked
+    // update the state -> likes
+    setLikes(likes + 1);
   }
 
-  // life cycle methods
-  componentDidMount() {
-    // this method is called after the component is mounted to the DOM
-    console.log('App: Component did mount');
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    // this method is called after the component is updated or re-rendered
-    console.log('App: Component did update');
-  }
-
-  handleLike = () => {
-    this.setState({
-      likes: this.state.likes + 1
-    })
-  }
-
-  handleHide = () => {
-    this.setState({
-      isHidden: !this.state.isHidden
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        <button className='showHideButton' onClick={this.handleHide}>
-          {
-            this.state.isHidden ? 'Show' : 'Hide'
-          }
-        </button>
-
-        {
-          !this.state.isHidden && (
-            <Card
-              likes={this.state.likes}
-              handleLike={this.handleLike}
-            />
-          ) 
-        }
-      </div>
-    )
-  }
+  return (
+    <div>
+      <h1>Likes: { likes }</h1>
+      <button onClick={handleLike}>Like</button>
+    </div>
+  )
 }
 
 export default App;
