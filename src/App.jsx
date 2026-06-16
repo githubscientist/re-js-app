20a904fc5909da474ef943c71690ed60e4d2ddd1
githubscientist <sathish.datascientist@gmail.com>
2026-06-16 20:22:03 +0530

    rendering api data

--- src/App.jsx
+++ src/App.jsx
@@ -18,10 +18,50 @@ const App = () => {
       })
   }, []);
   
-  console.log(wordDefinitions);
+  // console.log(wordDefinitions[0].sourceUrls[0]);
   
   return (
-    <div>App</div>
+    <>
+      {
+        wordDefinitions.length > 0 ? (
+          <div>
+            <h1>{wordDefinitions[0].word}</h1>
+      
+            <p>Dictionary</p>
+            Definitions from <a href={wordDefinitions[0].sourceUrls.length > 0 ? wordDefinitions[0].sourceUrls[0] : '#'}>{'wiktionary.org'}</a> . <a href="">Learn More</a>
+            
+            <div>
+              {
+                wordDefinitions[0].meanings.length > 0 ? (
+                  <div>
+                    {
+                      wordDefinitions[0].meanings
+                      .map((meaning, index) => {
+                        return <div key={index}>
+                          <p>{meaning.partOfSpeech}</p>
+                          
+                          <ol>
+                            {
+                              meaning.definitions.map((definition) => (
+                                <li>{ definition.definition }</li>
+                              ))
+                            }
+                          </ol>
+                        </div>
+                      })
+                    }
+                  </div>
+                ) : (
+                    <p>No Meanings found...</p>
+                )
+              }
+            </div>
+          </div>
+        ) : (
+            <p>Fetching Meaning...</p>
+        )
+      }
+    </>
   )
 }
 
