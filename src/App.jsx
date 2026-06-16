8d86ff81cef7a7766f16c32272d17293224694ee
githubscientist <sathish.datascientist@gmail.com>
2026-06-16 19:15:08 +0530

    making api requests using fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,23 @@
 const App = () => {
 
   // make an api call
-  // using XHR - XMLHttpRequest
-  // 1. Initialize the XHR instance.
-  const xhr = new XMLHttpRequest();
+  // using Fetch API
 
-  // 2. Define the URL
-  const url = 'https://api.dictionaryapi.dev/api/v2/entries/en/tree';
-
-  // 3. Open the Request
-  xhr.open('GET', url);
-
-  // 4. Attach Success and Error callbacks
-  // this will trigger after the api call succeeds or fails
-  xhr.onload = function () {
-    console.log(xhr.responseText);
-  }
-
-  xhr.onerror = function () {
-    console.log('API Call Failed!');
-  }
-
-  // 5. Send the Request
-  xhr.send();
-
-
+  // Make the request
+  fetch('https://api.dictionaryapi.dev/api/v2/entries/en/tree', {
+    method: 'GET'
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => {
+      console.log('API Call Failed!');
+      console.log(error);
+    })
+  
   return (
     <div>App</div>
   )
