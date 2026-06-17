7bf45883c22cee54e995241ce1df269b639df225
githubscientist <sathish.datascientist@gmail.com>
2026-06-17 18:58:51 +0530

    using a state variable --likes counter example

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,36 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const App = () => {
 
   const [doRender, setDoRender] = useState(0);
 
   /*
+    Ordinary Variable:
+
     1. The value gets updated
     2. The value update/change does not cause re-rendering
     3. The value will not persist across re-renders
+
+    State Variable:
+    1. The value gets updated
+    2. The value update/change cause re-rendering
+    3. The value will persist across re-renders
   */
 
-  let likes = 0;
+  let [likes, setLikes] = useState(0);
 
   const handleLike = () => {
-    likes++;
-    console.log(likes);
+    setLikes(likes + 1);
   }
 
   const handleReRender = () => {
     setDoRender(doRender + 1);
     console.log('Component re-rendered');
   }
+
+  useEffect(() => {
+    console.log(likes);
+  }, [likes]);
 
   return (
     <div>
