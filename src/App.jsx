32b1ddc6d62366830ec61a947af3f4566dd166d3
githubscientist <sathish.datascientist@gmail.com>
2026-06-22 18:34:39 +0530

    state management using useState hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,20 @@
+import { useState } from "react";
+
 const App = () => {
+
+  // add a state to handle the likes
+  const [likes, setLikes] = useState(0);
+
+  const handleLike = () => {
+    // setLikes((prev) => prev + 1);
+    setLikes(likes + 1);
+  };
+
   return (
-    <div>App</div>
+    <div>
+      <h1>Likes: { likes }</h1>
+      <button onClick={handleLike}>Like</button>
+    </div>
   )
 }
 
