a58cd2bb87590cf0c8a558df1bf38db353349bd8
githubscientist <sathish.datascientist@gmail.com>
2026-07-01 18:58:13 +0530

    intial repo setup --databases

--- src/main.jsx
+++
@@ -1,6 +0,0 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-// uncomment the following line for enabling tailwindcss
-import './index.css'
-
-createRoot(document.getElementById('root')).render(<App />);
